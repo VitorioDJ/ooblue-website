90cf0f29943a74035968400eefcfc5544dad7318
Vitorio De Janeiro <111264738+VitorioDJ@users.noreply.github.com>
2022-11-28 22:11:57 +0900

    Update form-processor.php

--- assets/php/form-processor.php
+++ assets/php/form-processor.php
@@ -164,7 +164,7 @@
 		echo
 		'<div class="alert alert-success alert-dismissible fade show" role="alert">
 			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			Your application has been sent successfully.
+			Your response has been sent successfully.
 		</div>
 		<div>
            <a class="btn btn-block btn-success btn-lg hvr-sweep-top mb-1" href="';
